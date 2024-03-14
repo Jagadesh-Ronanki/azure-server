5a17981bb8fec574b215730074d2281d6f432b56
Jagadesh <jagadesh.ronanki@gmail.com>
2024-03-14 12:08:44 +0530

    listening

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+ });
+ 
 
 // Configure SQL Server connection
 const config = {
